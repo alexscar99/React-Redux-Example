2dc7f4276a4bee7825e032cbfec88525a3cdb3ac
Alex Scarlett <alex.scarlett91@gmail.com>
2018-03-01 02:14:27 -0500

    added components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAE58JLujEcIy2931KPKovw6PIBhLtmSu0';
+
 // Create a new component. This component should produce
 // some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+    <div>
+        <SearchBar />    
+    </div>
+    );
 }
 
 // Take this component's generated HTML and put it
